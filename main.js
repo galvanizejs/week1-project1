7af73e73dcf9f43f2ecd8155e7ba9a0de8c62685
galvanizejs <js@matthewleach.com>
2016-03-02 18:49:36 -0700

    Spacing to improve readability

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 //Week 1 - Project 1
 
+
 // Log all answers to the console and write them to document
 // =======================================================
 
 //1. Log your name to the console (as a string).
 
-//2. Declare a variable called name, log it to the console.
+//2. Declare a variable called 'name', and then log it to the console.
 
-//3. Declare two variables with number values. Add, subtract, multiply and divide them.
+//3. Declare two variables with number values. 
+//   Add, subtract, multiply and divide them.
 
-//4. Declare two variables with string values. Create a third variable that concatenates them.
+//4. Declare two variables with string values. 
+//   Create a third variable that concatenates them.
 
-//5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
+//5. Add, subract, multiply and divide the strings '5' and '3'. 
+//   Observe the results and know how to explain the results if asked.
 
-//6. Create a prompt that asks a user to pick a number. Double the answer.
+//6. Create a prompt that asks a user to pick a number. 
+//   Then double the answer.
 
 //7. Create a custom alert.
 
-//8. Declare three variables called length, width and height. Calculate area and volume.
+//8. Declare three variables called length, width and height. 
+//   Calculate area and volume.
 
-//9. Ask for a user's name, age and city. In a single command display/log the sentence:
-// "My name is...I'm....years old, and I live in....."
+//9. Ask for a user's name, age and city. 
+//   In a single command display/log the sentence:
+//   "My name is...I'm....years old, and I live in....."
 
 //10. Create an alert that uses the response from a prompt.
+
+
