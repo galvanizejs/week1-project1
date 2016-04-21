55ce82810f02ed727c116bcb131981dabed48795
galvanizejs <js@matthewleach.com>
2016-04-21 14:52:26 -0600

    Updated Presentation & Minor Edits to Questions

--- main.js
+++ main.js
@@ -1,38 +1,103 @@
-//Week 1 - Project 1
+/*
+=======================================================
+
+** Week 1 - Project 1 **
+
+Below are a number of problems for you to solve 
+using JS. The JS code can be written below each
+problem and the result can be logged to the console. 
+
+In order to make the results easy to read, 
+please use console.log("Question #") to declare the 
+problem number being solved followed by console.log(yourAnswer) after each question.
+
+=======================================================
+*/
 
 
-// Log all answers to the console and write them to document
-// Please console.log("Question#") followed by console.log(yourAnswer) after each question.
-// =======================================================
 
-//1. Log your name to the console (as a string).
+// 1. Log your name to the console (as a string).
 
 console.log("Question 1");
-console.log(yourAnswer);
+console.log(answers-here);
 
-//2. Declare a variable called 'name', and then log it to the console.
 
-//3. Declare two variables with number values. 
-//   Add, subtract, multiply and divide them.
 
-//4. Declare two variables with string values. 
-//   Create a third variable that concatenates them.
 
-//5. Add, subract, multiply and divide the strings '5' and '3'. 
-//   Observe the results and know how to explain the results if asked.
+// 2. Declare a new variable called 'name', 
+//    and then log it to the console.
 
-//6. Create a prompt that asks a user to pick a number. 
-//   Then double the answer.
 
-//7. Create a custom alert.
 
-//8. Declare three variables called length, width and height. 
-//   Calculate area and volume.
 
-//9. Ask for a user's name, age and city. 
-//   In a single command display/log the sentence:
-//   "My name is...I'm....years old, and I live in....."
 
-//10. Create an alert that uses the response from a prompt.
+// 3. Declare two variables with number values. 
+//    Add, subtract, multiply and divide them.
+
+
+
+
+
+// 4. Declare two variables with string values. 
+//    Create a third variable that concatenates the two strings 
+//    that also places a space between the strings.
+
+
+
+
+
+// 5. Add, subract, multiply and divide the strings '5' and '3'. 
+//    Observe the results and brieftly explain the results within the console.
+
+
+
+
+
+// 6. Create a custom alert.
+
+
+
+
+
+// 7. Create a prompt that asks a user to pick a number. 
+//    Then double the answer.
+
+
+
+
+
+// 8. Ask for a user's name, age and city. 
+//    In a single command display/log the sentence:
+//    "My name is...I'm....years old, and I live in....."
+
+
+
+
+
+// 9. Create an alert that uses the response from a prompt.
+
+
+
+
+
+// 10. Create the formula to determine the area of a rectangle.
+//     The rectangle is: 2 inches by 3 inches
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
